c0939ce056d6819b018ef97be4bb38bb20f8dcc8
OhWhale515 <sterlinggreen515@gmail.com>
2023-04-30 06:54:21 -0400

    created client and for loop for console output

--- boto3-demo.py
+++ boto3-demo.py
@@ -2,10 +2,18 @@
 
 import boto3
 aws_mg_con=boto3.session.Session(profile_name='profile_name')
-iam_con=aws_mg_con.resource('iam')
+#iam_con=aws_mg_con.resource('iam')
 
 # list all iam users
 # for each user in iam_con.all():
 # print(each_user.name)
 
-print(dir(aws_mg_con))
+#example 
+print(aws_mg_con.get_available_resources()) 
+
+# client
+iam_con=aws_mg_con.client(service_name='iam', region_name='us-east-1')
+
+# for loop
+for each_user in iam_con.list_users()["User"]:
+    print(each_user['UserName'])
